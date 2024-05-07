2d81e750186b1b9e91d2b0050d6d923d93ce72fc
Ink-By <inkinkb@gmail.com>
2024-05-07 14:28:48 +0900

    init: set up the project

---
+++ src/main/java/com/choiteresa/fonation/FonationApplication.java
@@ -0,0 +1,13 @@
+package com.choiteresa.fonation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FonationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choiteresa/fonation/FonationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choiteresa.fonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
