5315296c8917566e0077c8d4738c32f578b22721
Ink-By <inkinkb@gmail.com>
2024-05-13 17:32:14 +0900

    feat: set up member entity

--- src/main/java/com/choiteresa/fonation/domain/member/entity/Member.java
+++ src/main/java/com/choiteresa/fonation/domain/member/entity/Member.java
@@ -1,5 +1,39 @@
 package com.choiteresa.fonation.domain.member.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member
 {
+    @Id
+    @Column(name = "member_id")
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String address;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phone_number;
+
+    @Column(nullable = false, unique = true)
+    private String password;
+
+    @Id
+    @Column(name = "role_id", nullable = false)
+    private int role_id;
+
+
+
+
+
+
 }
