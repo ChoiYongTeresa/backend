97ac65d221a3335487a130b32f8eb467ea97fa0e
0tae <choiyt3465@naver.com>
2024-05-16 22:25:46 +0900

    feature: implemented createFoodMarket(fetchFromRemoteConfig), fetchFoodMarket, fetchFoodMarketList(Sorted) api

--- src/main/java/com/choiteresa/fonation/domain/foodmarket/controller/FoodMarketController.java
+++ src/main/java/com/choiteresa/fonation/domain/foodmarket/controller/FoodMarketController.java
@@ -1,4 +1,46 @@
 package com.choiteresa.fonation.domain.foodmarket.controller;
 
-public class FoodmarketController {
+
+import com.choiteresa.fonation.domain.foodmarket.entity.FoodMarket;
+import com.choiteresa.fonation.domain.foodmarket.model.FetchFoodMarketRequestDto;
+import com.choiteresa.fonation.domain.foodmarket.model.FetchFoodMarketResponseDto;
+import com.choiteresa.fonation.domain.foodmarket.model.PreferFoodModel;
+import com.choiteresa.fonation.domain.foodmarket.service.FoodMarketService;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/donations/center")
+public class FoodMarketController {
+
+    private final FoodMarketService foodMarketService;
+
+
+    @GetMapping("/{marketId}")
+    public ResponseEntity fetchFoodMarket(@PathVariable Integer marketId){
+        return ResponseEntity.ok().body(foodMarketService.fetchFoodMarketById(marketId));
+    }
+    @GetMapping("/dataInit")
+    public ResponseEntity createFoodMarket() throws IOException, ParseException {
+        List<FoodMarket> foodMarkets =
+                foodMarketService.saveFoodMarketFromRemoteConfig();
+
+        return ResponseEntity.ok().body(foodMarkets);
+    }
+
+    @GetMapping("/readAll")
+    public ResponseEntity fetchFoodMarketList(@RequestBody FetchFoodMarketRequestDto dto) throws IOException, ParseException, InterruptedException {
+        List<FetchFoodMarketResponseDto> fetchFoodMarketResponseDtoList =
+                foodMarketService.fetchNearbyFoodMarketsSorted(dto);
+
+        return  ResponseEntity.ok().body(fetchFoodMarketResponseDtoList);
+    }
 }
+
+
