da80286fd310b96897fd9cc769ea77bebd43980a
Ink-By <inkinkb@gmail.com>
2024-05-27 15:42:17 +0900

    fix: set up ProductDonationForm entity

--- src/main/java/com/choiteresa/fonation/domain/product_donation_form/entity/ProductDonationForm.java
+++ src/main/java/com/choiteresa/fonation/domain/product_donation_form/entity/ProductDonationForm.java
@@ -2,7 +2,13 @@ package com.choiteresa.fonation.domain.product_donation_form.entity;
 
 import com.choiteresa.fonation.domain.member.entity.Member;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
 @Entity
 public class ProductDonationForm {
     //id, donation_user_id, is_selected
@@ -19,5 +25,13 @@ public class ProductDonationForm {
     @Column(nullable = false)
     private boolean isSelected;
 
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
+
+    public void setIsSelected(boolean b) {
+        isSelected = b;
+    }
 }
+
