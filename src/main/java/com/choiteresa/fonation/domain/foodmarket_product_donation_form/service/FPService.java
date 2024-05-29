3d00bdc60fc080fef1317e53387bc91180d46372
danhyunjo <jdh33114@naver.com>
2024-05-29 23:13:41 +0900

    feat: develop market selection

--- src/main/java/com/choiteresa/fonation/domain/foodmarket_product_donation_form/service/FPService.java
+++ src/main/java/com/choiteresa/fonation/domain/foodmarket_product_donation_form/service/FPService.java
@@ -2,9 +2,7 @@ package com.choiteresa.fonation.domain.foodmarket_product_donation_form.service;
 
 import com.choiteresa.fonation.domain.foodmarket.entity.FoodMarket;
 import com.choiteresa.fonation.domain.foodmarket.repository.FoodMarketRepository;
-import com.choiteresa.fonation.domain.foodmarket_product_donation_form.dto.SelectedProductDto;
-import com.choiteresa.fonation.domain.foodmarket_product_donation_form.dto.SelectedProductResponseDto;
-import com.choiteresa.fonation.domain.foodmarket_product_donation_form.dto.SelectedProductRequestDto;
+import com.choiteresa.fonation.domain.foodmarket_product_donation_form.dto.*;
 import com.choiteresa.fonation.domain.foodmarket_product_donation_form.entity.FoodmarketProductRelation;
 import com.choiteresa.fonation.domain.foodmarket_product_donation_form.repository.FPRepository;
 import com.choiteresa.fonation.domain.product.entity.Product;
@@ -17,6 +15,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -56,6 +55,19 @@ public class FPService {
         }
 
         return result;
+
+    }
+    public MarketSelectionResponseDto selectMarket(MarketSelectionRequestDto selectRequest){
+        Optional<FoodmarketProductRelation> relationOptional = FPRepository.findByFoodMarketIdAndDonationFormId(
+                selectRequest.getFoodMarketId(), selectRequest.getDonationFormId());
+        if (relationOptional.isPresent()) {
+            FoodmarketProductRelation relation = relationOptional.get();
+            relation.setSelectedDate(new Date());
+            FPRepository.save(relation);
+            return MarketSelectionResponseDto.fromEntity(1);
+        } else {
+            return MarketSelectionResponseDto.fromEntity(-1);
+        }
 
     }
 
