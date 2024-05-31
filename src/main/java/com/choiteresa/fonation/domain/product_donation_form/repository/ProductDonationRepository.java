b89b6c9a62fbee466197cd153ac2126319549351
Ink-By <inkinkb@gmail.com>
2024-05-31 16:11:49 +0900

    feat: 상태로 얻어옴

--- src/main/java/com/choiteresa/fonation/domain/product_donation_form/repository/ProductDonationRepository.java
+++ src/main/java/com/choiteresa/fonation/domain/product_donation_form/repository/ProductDonationRepository.java
@@ -1,4 +1,13 @@
 package com.choiteresa.fonation.domain.product_donation_form.repository;
 
-public class ProductDonationRepository {
+import com.choiteresa.fonation.domain.product_donation_form.entity.ProductDonationForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductDonationRepository extends JpaRepository<ProductDonationForm, Long> {
+    List<ProductDonationForm> findByStatus(String status);
+
 }
