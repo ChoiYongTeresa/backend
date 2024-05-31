e102dac1bd5b8bb9a45b06062bd2b928c4e016fe
Ink-By <inkinkb@gmail.com>
2024-05-31 16:11:12 +0900

    feat: Product 상태 업데이트

--- src/main/java/com/choiteresa/fonation/domain/product/service/ProductService.java
+++ src/main/java/com/choiteresa/fonation/domain/product/service/ProductService.java
@@ -1,4 +1,17 @@
 package com.choiteresa.fonation.domain.product.service;
 
+import com.choiteresa.fonation.domain.product_donation_form.entity.ProductDonationForm;
+import com.choiteresa.fonation.domain.product_donation_form.repository.ProductDonationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+    @Autowired
+    private ProductDonationRepository formRepository;
+
+    public ProductDonationForm submitDonationForm(ProductDonationForm form) {
+        form.setStatus("WAITING");
+        return formRepository.save(form);
+    }
 }
