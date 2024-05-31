17ba56766dc89405c1c82c9d56a3e2a88d3e2cf3
Ink-By <inkinkb@gmail.com>
2024-05-31 16:20:31 +0900

    feat: repository annotation

--- src/main/java/com/choiteresa/fonation/domain/foodmarket_product_donation_form/repository/FPRepository.java
+++ src/main/java/com/choiteresa/fonation/domain/foodmarket_product_donation_form/repository/FPRepository.java
@@ -1,4 +1,7 @@
 package com.choiteresa.fonation.domain.foodmarket_product_donation_form.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class FPRepository {
 }
