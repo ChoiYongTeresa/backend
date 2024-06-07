c2adc0988462b5b229d45d1e7e28dd7972968788
Ink-By <inkinkb@gmail.com>
2024-06-07 17:44:07 +0900

    fix: 릴레이션 엔티티 수정

--- src/main/java/com/choiteresa/fonation/domain/foodmarket_product_donation_form/entity/FoodmarketProductRelation.java
+++ src/main/java/com/choiteresa/fonation/domain/foodmarket_product_donation_form/entity/FoodmarketProductRelation.java
@@ -1,11 +1,9 @@
 package com.choiteresa.fonation.domain.foodmarket_product_donation_form.entity;
 
-import com.choiteresa.fonation.domain.foodmarket.entity.FoodMarket;
 import com.choiteresa.fonation.domain.product_donation_form.entity.ProductDonationForm;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
@@ -13,23 +11,24 @@ import java.util.Date;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class FoodmarketProductRelation {
-// id, donation_form_id, foodmarket_id, selected_date
-
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     public Integer id;
 
-    // FPR many : donation_form one
-    @JoinColumn(name = "donation_form_id")
     @ManyToOne
-    public ProductDonationForm donationFormId;
+    @JoinColumn(name = "product_donation_form_id", nullable = false)
+    public ProductDonationForm productDonationForm;
 
-    @ManyToOne
-    @JoinColumn(name = "foodmarket_id")
-    public FoodMarket foodMarketId;
-
+    private Long foodMarketId;  // 이름 변경
     private Date selectedDate;
+
+    public FoodmarketProductRelation() {}
+
+    public FoodmarketProductRelation(ProductDonationForm donationForm, Long foodMarketId) {  // 파라미터 이름 변경
+        this.productDonationForm = donationForm;
+        this.foodMarketId = foodMarketId;  // 필드 이름 변경
+        this.selectedDate = null;
+    }
 }
