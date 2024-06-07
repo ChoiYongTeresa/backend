ee122c1e20ebc3157b00da5cbbb31feadceaf3c1
Ink-By <inkinkb@gmail.com>
2024-06-07 17:47:17 +0900

    fix: product service

--- src/main/java/com/choiteresa/fonation/domain/product/service/ProductService.java
+++ src/main/java/com/choiteresa/fonation/domain/product/service/ProductService.java
@@ -1,16 +1,40 @@
 package com.choiteresa.fonation.domain.product.service;
 
+import com.choiteresa.fonation.domain.product.entity.Product;
+import com.choiteresa.fonation.domain.product.repository.ProductRepository;
 import com.choiteresa.fonation.domain.product_donation_form.entity.ProductDonationForm;
 import com.choiteresa.fonation.domain.product_donation_form.repository.ProductDonationRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductDonationRepository formRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Transactional
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Transactional
+    public List<Product> saveAllProducts(List<Product> products) {
+        return productRepository.saveAll(products);
+    }
+
 
     public ProductDonationForm submitDonationForm(ProductDonationForm form) {
+        // TODO : ?? ???? ??, ??? ???? ??????? ????? ???? ??????.
         form.setStatus("WAITING");
         return formRepository.save(form);
     }
