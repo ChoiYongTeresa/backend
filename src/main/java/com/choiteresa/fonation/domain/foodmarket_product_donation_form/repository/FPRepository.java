941e4c0be4befd86c88eb470f1c7961d310c81be
Ink-By <inkinkb@gmail.com>
2024-06-07 17:44:28 +0900

    fix: FPRepository.java

--- src/main/java/com/choiteresa/fonation/domain/foodmarket_product_donation_form/repository/FPRepository.java
+++ src/main/java/com/choiteresa/fonation/domain/foodmarket_product_donation_form/repository/FPRepository.java
@@ -1,7 +1,12 @@
 package com.choiteresa.fonation.domain.foodmarket_product_donation_form.repository;
 
+import com.choiteresa.fonation.domain.foodmarket_product_donation_form.entity.FoodmarketProductRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class FPRepository {
+public interface FPRepository extends JpaRepository<FoodmarketProductRelation, Long>{
+//    List<FoodmarketProductRelation> findByFoodmarketId(Long foodmarketId);
+//    List<FoodmarketProductRelation> findByDonationFormId(Long donationFormId);
 }
+
