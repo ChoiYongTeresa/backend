0113cc567e82c62dd2881e4696b740603092e842
Ink-By <inkinkb@gmail.com>
2024-06-09 15:08:25 +0900

    feat: 기부 신청서 불러오기 리스트

--- src/main/java/com/choiteresa/fonation/domain/admin_approval/service/AdminApprovalService.java
+++ src/main/java/com/choiteresa/fonation/domain/admin_approval/service/AdminApprovalService.java
@@ -1,7 +1,32 @@
 package com.choiteresa.fonation.domain.admin_approval.service;
 
+import com.choiteresa.fonation.domain.admin_approval.Dto.DonationRequestDto;
+import com.choiteresa.fonation.domain.product_donation_form.repository.ProductDonationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AdminApprovalService {
+    @Autowired
+    private ProductDonationRepository productDonationRepository;
+    public List<DonationRequestDto> getAllDonationRequests() {
+        return productDonationRepository.findAll().stream()
+                .flatMap(form -> form.getRelations().stream())
+                .flatMap(relation -> relation.getProducts().stream().map(product -> {
+                    DonationRequestDto dto = new DonationRequestDto();
+                    dto.setName(relation.getProductDonationForm().getDonationUserId().getMemberName());
+                    dto.setPhone(relation.getProductDonationForm().getDonationUserId().getPhoneNumber());
+                    dto.setEmail(relation.getProductDonationForm().getDonationUserId().getEmail());
+                    dto.setProductCategory(product.getCategory());
+                    dto.setProductName(product.getName());
+                    dto.setProductNum(product.getQuantity());
+                    dto.setExpireDate(product.getExpireDate());
+                    dto.setProductStorage(product.getStoreType());
+                    return dto;
+                }))
+                .collect(Collectors.toList());
+    }
 }
