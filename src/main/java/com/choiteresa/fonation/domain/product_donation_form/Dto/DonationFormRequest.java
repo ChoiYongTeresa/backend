558e54d125e6ade713e0709939be6ff35cd6fc26
Ink-By <inkinkb@gmail.com>
2024-06-09 15:08:43 +0900

    feat: 기부 신청서 요청 수정

--- src/main/java/com/choiteresa/fonation/domain/product_donation_form/Dto/DonationFormRequest.java
+++ src/main/java/com/choiteresa/fonation/domain/product_donation_form/Dto/DonationFormRequest.java
@@ -1,5 +1,6 @@
 package com.choiteresa.fonation.domain.product_donation_form.Dto;
 
+import com.choiteresa.fonation.domain.member.entity.Member;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,21 @@ import java.util.List;
 @Getter
 @Setter
 public class DonationFormRequest {
-    private Long donationUserId;
+    private Member donationUserId;
     private List<ProductRequest> productList;
+    private List<Integer> foodMarketIds;
+
+
+
+    @Getter
+    public static class ProductRequest {
+        private String category;
+        private String name;
+        private int quantity;
+        private String expireDate;
+        private int storeType;
+        private int weight;
+        private boolean isSelected;
+
+    }
 }
