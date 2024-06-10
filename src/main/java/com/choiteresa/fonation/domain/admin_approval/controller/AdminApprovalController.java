b5428d2727591ff3285be354a625ce88bb7651e8
Ink-By <inkinkb@gmail.com>
2024-06-10 19:52:41 +0900

    fix: bug fix, 기부 신청서 불러오기, 승인, 거부

--- src/main/java/com/choiteresa/fonation/domain/admin_approval/controller/AdminApprovalController.java
+++ src/main/java/com/choiteresa/fonation/domain/admin_approval/controller/AdminApprovalController.java
@@ -3,6 +3,7 @@ package com.choiteresa.fonation.domain.admin_approval.controller;
 import com.choiteresa.fonation.domain.product_donation_form.entity.ProductDonationForm;
 import com.choiteresa.fonation.domain.product_donation_form.service.ProductDonationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,18 +32,41 @@ public class AdminApprovalController {
         this.productDonationService = productDonationService;
     }
     @GetMapping("/{donationId}")
-    public ResponseEntity<ProductDonationForm> getForm(@PathVariable Long id) {
-        // TODO : 도네이션 폼을 얻어오는 API
-        return ResponseEntity.ok(productDonationService.getForm(id));
+    public ResponseEntity<?> getForm(@PathVariable("donationId") Long id) {
+        try {
+            ProductDonationForm form = productDonationService.getForm(id);
+            return ResponseEntity.ok(form);
+        } catch (Exception e) {
+            // 로깅을 추가하여 오류의 원인을 추적할 수 있도록 합니다.
+            // 예를 들어, 로그에 오류 정보를 출력
+            System.err.println("Error retrieving donation form: " + e.getMessage());
+            e.printStackTrace(); // 서버 로그에 출력
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error retrieving donation form");
+        }
     }
+
+//    @PostMapping("/{donationId}/approval")
+//    public ResponseEntity<ProductDonationForm> approveForm(@PathVariable Long id) {
+//        // TODO : 도네이션 폼을 승인하는 API
+//        return ResponseEntity.ok(productDonationService.approveForm(id));
+//    }
+//
+//    @PostMapping("/{donationId}/reject")
+//    public ResponseEntity<ProductDonationForm> rejectForm(@PathVariable Long id) {
+//        // TODO : 도네이션 폼을 거절하는 API
+//        return ResponseEntity.ok(productDonationService.rejectForm(id));
+//    }
     @PostMapping("/{donationId}/approval")
-    public ResponseEntity<ProductDonationForm> approveForm(@PathVariable Long id) {
+    public ResponseEntity<ProductDonationForm> approveForm(@PathVariable("donationId") Long id) {
+    // TODO : 도네이션 폼을 승인하는 API
         return ResponseEntity.ok(productDonationService.approveForm(id));
     }
 
     @PostMapping("/{donationId}/reject")
-    public ResponseEntity<ProductDonationForm> rejectForm(@PathVariable Long id) {
-
+    public ResponseEntity<ProductDonationForm> rejectForm(@PathVariable("donationId") Long id) {
+        // TODO : 도네이션 폼을 거절하는 API
         return ResponseEntity.ok(productDonationService.rejectForm(id));
     }
+
 }
--- src/main/java/com/choiteresa/fonation/domain/product_donation_form/entity/ProductDonationForm.java
+++ src/main/java/com/choiteresa/fonation/domain/product_donation_form/entity/ProductDonationForm.java
@@ -2,6 +2,7 @@ package com.choiteresa.fonation.domain.product_donation_form.entity;
 
 import com.choiteresa.fonation.domain.foodmarket_product_donation_form.entity.FoodmarketProductRelation;
 import com.choiteresa.fonation.domain.member.entity.Member;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,6 +17,7 @@ import java.util.List;
 @Table(name = "product_donation_form")
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(value = {"relations"})
 public class ProductDonationForm {
     //id, donation_user_id, is_selected
     @Id
