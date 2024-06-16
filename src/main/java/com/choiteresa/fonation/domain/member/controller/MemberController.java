e9d03291af756462738761ce16c357056b1ffbef
Ink-By <inkinkb@gmail.com>
2024-06-16 16:44:15 +0900

    fix: 기능 누락 수정

--- src/main/java/com/choiteresa/fonation/domain/member/controller/MemberController.java
+++ src/main/java/com/choiteresa/fonation/domain/member/controller/MemberController.java
@@ -4,7 +4,6 @@ import com.choiteresa.fonation.domain.member.dto.*;
 import com.choiteresa.fonation.domain.member.entity.Member;
 import com.choiteresa.fonation.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -20,36 +19,39 @@ public class MemberController {
 
     @PostMapping("/register/user")
     @ResponseBody
-    public ResponseEntity<UserRegisterResponseDto> registerUser(@RequestBody UserRegisterRequestDto memberDto){
+    public ResponseEntity<UserRegisterResponseDto> registerUser(@RequestBody UserRegisterRequestDto memberDto) {
         return ResponseEntity.ok(
                 memberService.registerUserMember(memberDto));
     }
+
     @PostMapping("/register/admin")
     @ResponseBody
-    public ResponseEntity<AdminRegisterResponseDto> registerAdmin(@RequestBody AdminRegisterRequestDto memberDto){
+    public ResponseEntity<AdminRegisterResponseDto> registerAdmin(@RequestBody AdminRegisterRequestDto memberDto) {
         return ResponseEntity.ok(
                 memberService.registerAdminMember(memberDto));
     }
 
     @PostMapping("/login/user")
-    public ResponseEntity<UserLoginResponseDto> loginUser(@RequestBody UserLoginRequestDto loginRequest){
+    public ResponseEntity<UserLoginResponseDto> loginUser(@RequestBody UserLoginRequestDto loginRequest) {
         return ResponseEntity.ok(
                 memberService.loginUser(loginRequest));
     }
+
     @PostMapping("/login/admin")
-    public ResponseEntity<AdminLoginResponseDto> loginAdmin(@RequestBody AdminLoginRequestDto loginRequest){
+    public ResponseEntity<AdminLoginResponseDto> loginAdmin(@RequestBody AdminLoginRequestDto loginRequest) {
         return ResponseEntity.ok(
                 memberService.loginAdmin(loginRequest));
     }
+
     @GetMapping("/summary/{member_id}")
-    public ResponseEntity<SummaryResponseDto> summaryMemerInfo(@RequestBody SummaryRequestDto summaryRequest){
+    public ResponseEntity<SummaryResponseDto> summaryMemerInfo(@RequestBody SummaryRequestDto summaryRequest) {
         Optional<Member> findMember = memberService.summaryMemberInfo(summaryRequest);
 
-        if(findMember.isPresent()){
+        if (findMember.isPresent()) {
             SummaryDto summary = SummaryDto.fromEntity(findMember.get());
-            return ResponseEntity.ok(SummaryResponseDto.fromEntity(1,summary));
-        } else{
-            return ResponseEntity.ok(SummaryResponseDto.fromEntity(-1,null));
+            return ResponseEntity.ok(SummaryResponseDto.fromEntity(1, summary));
+        } else {
+            return ResponseEntity.ok(SummaryResponseDto.fromEntity(-1, null));
         }
     }
 }
--- src/main/java/com/choiteresa/fonation/domain/product_donation_form/controller/ProductDonationController.java
+++ src/main/java/com/choiteresa/fonation/domain/product_donation_form/controller/ProductDonationController.java
@@ -1,7 +1,12 @@
 package com.choiteresa.fonation.domain.product_donation_form.controller;
 
+import com.choiteresa.fonation.domain.product_donation_form.Dto.DonationFormRequest;
+import com.choiteresa.fonation.domain.product_donation_form.Dto.DonationResponse;
 import com.choiteresa.fonation.domain.product_donation_form.service.ProductDonationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +17,10 @@ public class ProductDonationController {
         this.productDonationService = service;
     }
 
+    @PostMapping("/donations/product/donationForm")
+    public ResponseEntity<?> submitDonationForm(@RequestBody DonationFormRequest request) {
+        // TODO : 도네이션 폼을 제출하는 API
+        DonationResponse response = productDonationService.createDonation(request);
+        return ResponseEntity.ok(response);
+    }
 }
